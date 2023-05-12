9f0d4fbd74ab21d84282cd0335f2c3e141fae467
Inieke Sendy <ini.e.sendy@gmail.com>
2023-05-12 00:43:38 -0500

    [feat-init]: Update: Index.ts with new components and images with new images

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,6 +1,22 @@
-import Button from "./Button";
-import Background from "./Background";
-import BackButton from "./BackButton";
-import TextComponent from "./TextComponent";
+import Button from './Button';
+import Background from './Background';
+import BackButton from './BackButton';
+import TextComponent from './TextComponent';
+import Loader from './Loader';
+import Logo from './Logo';
+import CustomInput, { DropDownInput } from './CustomInput';
+import Line from './Line';
+import TextLinks from './TextLinks';
 
-export { Button, Background, BackButton, TextComponent };
+export {
+  Button,
+  Background,
+  BackButton,
+  TextComponent,
+  Loader,
+  Logo,
+  CustomInput,
+  DropDownInput,
+  Line,
+  TextLinks,
+};
--- src/global/images.ts
+++ src/global/images.ts
@@ -1,4 +1,5 @@
 const landingPageBackgroundImage = require('../assets/images/user_bk.jpg');
 const whiteLogo = require('../assets/images/logo_cpl.png');
+const loginPageBackgroundImage = require('../assets/images/user_ym.jpg');
 
-export { landingPageBackgroundImage, whiteLogo };
+export { landingPageBackgroundImage, whiteLogo, loginPageBackgroundImage };
