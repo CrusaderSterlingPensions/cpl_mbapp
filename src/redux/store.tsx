020c5c78566d95cc16a1aaf35e5d6489d628757a
Inieke Sendy <ini.e.sendy@gmail.com>
2023-05-13 11:23:41 -0500

    [feat-init]: update: add usersreducer to Store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,8 +1,10 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
+import userReducer from './userSlice';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    user: userReducer,
   },
 });
