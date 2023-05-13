029a164974034dde9608ab320e9c4cc103f8320a
Inieke Sendy <ini.e.sendy@gmail.com>
2023-05-13 11:27:15 -0500

    [feat-init]: Update: Landing Screen with Video

--- src/screens/LandingScreen.tsx
+++ src/screens/LandingScreen.tsx
@@ -1,5 +1,5 @@
 import { Image, SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { LinearGradient } from 'expo-linear-gradient';
 import { Background, Button } from '../components';
 import { COLORS, SIZES } from '../global';
@@ -7,8 +7,28 @@ import { ImageBackground } from 'react-native';
 import { landingPageBackgroundImage, whiteLogo } from '../global/images';
 import { StatusBar } from 'expo-status-bar';
 import { moderateScale } from 'react-native-size-matters';
+import { Video } from 'expo-av';
 
-const LandingScreen = ({ navigation }: any) => {
+const LandingScreen = ({ navigation, onFinish }: any) => {
+  const [status, setStatus] = useState({});
+  const [videoComplete, setVideoComplete] = useState(false);
+
+  const videoRef = useRef<Video>(null);
+  useEffect(() => {
+    const video = videoRef.current;
+
+    const onPlaybackStatusUpdate = (playbackStatus: any) => {
+      if (playbackStatus.didJustFinish) {
+        setVideoComplete(true);
+      }
+    };
+
+    video?.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+
+    return () => {
+      video?.setOnPlaybackStatusUpdate(null);
+    };
+  }, []);
   return (
     <View>
       <StatusBar style="light" />
@@ -17,38 +37,57 @@ const LandingScreen = ({ navigation }: any) => {
           height: SIZES.height,
         }}
       >
-        <ImageBackground
-          source={landingPageBackgroundImage}
-          style={{ flex: 1, justifyContent: 'flex-end' }}
-          resizeMode="cover"
-        >
-          <LinearGradient
-            colors={[COLORS.NEUTRAL.TRANSPARENT20, COLORS.NEUTRAL.TRANSPARENT60]}
-            style={{
-              height: SIZES.height,
-              width: SIZES.width,
-            }}
+        {videoComplete ? (
+          <ImageBackground
+            source={landingPageBackgroundImage}
+            style={{ flex: 1, justifyContent: 'flex-end' }}
+            resizeMode="cover"
           >
-            <SafeAreaView>
-              <View style={styles.container}>
-                <Image source={whiteLogo} style={styles.whiteLogo} />
-                <View>
-                  <Button
-                    text="Login"
-                    onPress={() => {
-                      navigation.navigate('Login');
-                    }}
-                  />
-                  <Button
-                    text="Open Pensions Account"
-                    onPress={() => {}}
-                    customBtnStyle={styles.customBtnStyle}
-                  />
-                </View>
-              </View>
-            </SafeAreaView>
-          </LinearGradient>
-        </ImageBackground>
+            <LinearGradient
+              colors={[COLORS.NEUTRAL.TRANSPARENT20, COLORS.NEUTRAL.TRANSPARENT60]}
+              style={{
+                height: SIZES.height,
+                width: SIZES.width,
+              }}
+            ></LinearGradient>
+          </ImageBackground>
+        ) : (
+          <Video
+            ref={videoRef}
+            source={require('../assets/videos/video_bk.mp4')}
+            useNativeControls
+            resizeMode="cover"
+            shouldPlay
+            isLooping
+            isMuted={true}
+            onPlaybackStatusUpdate={(status) => {
+              if (status.isLoaded && status.positionMillis === status.playableDurationMillis) {
+                onFinish();
+              }
+            }}
+            style={{ flex: 1, height: '100%', width: SIZES.width }}
+          />
+        )}
+        <View style={styles.whiteLogoContainer}>
+          <Image source={whiteLogo} style={styles.whiteLogo} />
+        </View>
+        <View style={styles.container}>
+          <View>
+            <Button
+              text="Login"
+              onPress={() => {
+                navigation.navigate('Login');
+              }}
+            />
+            <Button
+              text="Open Pensions Account"
+              onPress={() => {
+                navigation.navigate('OpenAccount');
+              }}
+              customBtnStyle={styles.customBtnStyle}
+            />
+          </View>
+        </View>
       </View>
     </View>
   );
@@ -57,6 +96,14 @@ const LandingScreen = ({ navigation }: any) => {
 export default LandingScreen;
 
 const styles = StyleSheet.create({
+  whiteLogoContainer: {
+    position: 'absolute',
+    top: '13%',
+    left: 0,
+    width: SIZES.width,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
   whiteLogo: {
     height: 50,
     width: 50,
@@ -68,6 +115,9 @@ const styles = StyleSheet.create({
     height: '100%',
     paddingVertical: moderateScale(50),
     paddingHorizontal: moderateScale(20),
+    position: 'absolute',
+    top: '65%',
+    left: 0,
   },
   customBtnStyle: {
     backgroundColor: COLORS.NEUTRAL.TRANSPARENT_WHITE20,
