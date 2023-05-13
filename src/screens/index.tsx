0cb3e8c4be412a9d1bf021cb7d2eba55745a8090
Inieke Sendy <ini.e.sendy@gmail.com>
2023-05-13 11:26:28 -0500

    [feat-init]: update: Screen Indexing

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,5 +1,29 @@
 import LandingScreen from './LandingScreen';
 import Login from './Login';
 import SplashVideo from './SplashVideo';
+import OpenAccount from './OpenAccount';
+import BenefitApplication from './BenefitApplication';
+import BranchLocator from './BranchLocator';
+import MandateChange from './MandateChange';
+import ProductCatalogue from './ProductCatalogue';
+import StatementRequest from './StatementRequest';
+import FundTransfer from './FundTransfer';
+import DataRecapture from './DataRecapture';
+import Calculator from './Calculator';
+import DrawerMenu from './Drawer';
 
-export { LandingScreen, Login, SplashVideo };
+export {
+  LandingScreen,
+  Login,
+  SplashVideo,
+  OpenAccount,
+  BenefitApplication,
+  BranchLocator,
+  MandateChange,
+  ProductCatalogue,
+  StatementRequest,
+  FundTransfer,
+  DataRecapture,
+  Calculator,
+  DrawerMenu,
+};
