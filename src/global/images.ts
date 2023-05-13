4275630a6bb4e9463d9c1349dff400c3b5fb2312
Inieke Sendy <ini.e.sendy@gmail.com>
2023-05-13 11:23:12 -0500

    [feat-init]: update: color,data, images and update index

--- src/global/images.ts
+++ src/global/images.ts
@@ -1,5 +1,21 @@
 const landingPageBackgroundImage = require('../assets/images/user_bk.jpg');
 const whiteLogo = require('../assets/images/logo_cpl.png');
 const loginPageBackgroundImage = require('../assets/images/user_ym.jpg');
+const signUpPageBackgroundImage = require('../assets/images/img_bur_two.jpg');
+const menuIcon = require('../assets/images/drawer_icon_2.png');
+const menuIconDark = require('../assets/images/drawer_icon_3.png');
+const logoRoundRed = require('../assets/images/logo_round_red.png');
+const nairaLogoWhite = require('../assets/images/naira_logo.png');
+const nairaLogoBlack = require('../assets/images/naira_logo_black.png');
 
-export { landingPageBackgroundImage, whiteLogo, loginPageBackgroundImage };
+export {
+  landingPageBackgroundImage,
+  whiteLogo,
+  loginPageBackgroundImage,
+  signUpPageBackgroundImage,
+  menuIcon,
+  menuIconDark,
+  logoRoundRed,
+  nairaLogoWhite,
+  nairaLogoBlack,
+};
--- src/global/index.ts
+++ src/global/index.ts
@@ -1,6 +1,7 @@
-import { COLORS } from "./colors";
-import { FONTS } from "./fonts";
-import { normalize } from "./sizes";
-import { SIZES } from "./sizes";
+import { COLORS } from './colors';
+import { FONTS } from './fonts';
+import { normalize } from './sizes';
+import { SIZES } from './sizes';
+import { data } from './data';
 
-export { COLORS, SIZES, normalize, FONTS };
+export { COLORS, SIZES, normalize, FONTS, data };
