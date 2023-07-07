2b43778d4f517b54387c5bad07d77935510c64cd
Sendy Inieke <ini.e.sendy@gmail.com>
2023-07-07 03:08:59 +0100

    [feat-statement]:update: update store with servicesReducer

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,12 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import authReducer from './authSlice';
 import userReducer from './userSlice';
+import servicesReducer from './servicesSlices';
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     user: userReducer,
+    services: servicesReducer,
   },
 });
