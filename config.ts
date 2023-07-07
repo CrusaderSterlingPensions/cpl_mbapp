cea30e2eaec50662cff9188ff21f4b545884d680
Sendy Inieke <ini.e.sendy@gmail.com>
2023-07-07 03:07:28 +0100

    [feat-statement]:update: config file

--- config.ts
+++ config.ts
@@ -1,5 +1,7 @@
 const BASE_API_URL = 'http://154.65.4.197:9912/CrusaderWebService/crusader/pensions';
+// const BASE_API_URL = 'http://154.65.4.193:9912/CrusaderWebService/crusader/pensions';
+const BASE_STATEMENT_API = 'http://132.10.100.221:9912/crusader/webservices/api/statement';
 const USERNAME = 'cr8der';
 const PASSWORD = 'p@sWrd9';
 
-export { BASE_API_URL, USERNAME, PASSWORD };
+export { BASE_API_URL, USERNAME, PASSWORD, BASE_STATEMENT_API };
