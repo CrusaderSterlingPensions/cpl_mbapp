d6041adf876ed7f54a09b3bade307ca0310e3c6e
Sendy Inieke <ini.e.sendy@gmail.com>
2023-07-08 12:55:00 +0100

    [feat-statement]:update: UX performance by adding scrollview

--- src/screens/FundTransfer.tsx
+++ src/screens/FundTransfer.tsx
@@ -10,16 +10,26 @@ import {
   StatusBar,
   ImageBackground,
   Linking,
+  ScrollView,
 } from 'react-native';
 import React, { useState, useEffect } from 'react';
 import { moderateScale, verticalScale } from 'react-native-size-matters';
 import { FONTS, SIZES, COLORS } from '../global';
-import { BackButton, Button, CustomInput, Line, Loader, Logo, TextLinks } from '../components';
+import {
+  BackButton,
+  Button,
+  CustomInput,
+  Line,
+  Loader,
+  Logo,
+  ModalScreen,
+  TextLinks,
+} from '../components';
 import { useDispatch, useSelector } from 'react-redux';
 import { LinearGradient } from 'expo-linear-gradient';
 import { landingPageBackgroundImage, loginPageBackgroundImage } from '../global/images';
-import { FontAwesome5, Entypo, EvilIcons } from '@expo/vector-icons';
 import Checkbox from 'expo-checkbox';
+import { FontAwesome5, Entypo, EvilIcons, MaterialIcons, Ionicons } from '@expo/vector-icons';
 
 const FundTransfer = ({ navigation }: any) => {
   type errorObjProps = {
@@ -39,6 +49,11 @@ const FundTransfer = ({ navigation }: any) => {
   const [emailChecked, setEmailChecked] = useState<boolean>(false);
   const [phoneChecked, setPhoneChecked] = useState<boolean>(false);
 
+  const [statementResponseData, setStatementResponseData] = useState<any>({});
+  const [statementModalSuccess, setStatementModalSuccess] = useState<boolean>(false);
+  const [statementModalError, setStatementModalError] = useState<boolean>(false);
+  const [dataLoading, setDataLoading] = useState<boolean>(false);
+
   const validate = async () => {
     Keyboard.dismiss();
     setIsValid(true);
@@ -50,10 +65,13 @@ const FundTransfer = ({ navigation }: any) => {
       handleError('Please provide Fund Type', 'fundChoice');
       setIsValid(false);
     }
-    if (isValid) {
-      alert('Statement Requested Successfully');
-    }
   };
+
+  useEffect(() => {
+    if (isValid) {
+      setStatementModalSuccess(true);
+    }
+  }, [isValid]);
 
   const handleError = (error: any, input: string) => {
     setErrors((prevState) => ({ ...prevState, [input]: error }));
@@ -64,74 +82,99 @@ const FundTransfer = ({ navigation }: any) => {
         height: SIZES.height,
       }}
     >
-      <SafeAreaView style={styles.wrapper}>
-        <View style={styles.container}>
-          <Text style={{ ...FONTS.body4Bold, marginVertical: moderateScale(10) }}>
-            Please kindly fill the form below appropiately
-          </Text>
-          <CustomInput
-            placeholder={'Reason for Withdrawal'}
-            multiline={true}
-            onChangeText={(text: string) => setWithdrawalReason(text)}
-            autoCapitalize="none"
-            onFocus={() => handleError(null, 'withdrawalReason')}
-            error={errors.withdrawalReason}
-            editable={true}
-            dark={true}
-          />
-          <Text style={{ ...FONTS.body4Bold, marginTop: moderateScale(10) }}>To</Text>
-          <CustomInput
-            placeholder={'Fund Type'}
-            onChangeText={(text: string) => setFundChoice(text)}
-            autoCapitalize="none"
-            onFocus={() => handleError(null, 'fundChoice')}
-            error={errors.fundChoice}
-            editable={true}
-            dark={true}
-          />
+      <ModalScreen
+        icon={
+          <Ionicons name="checkmark-done-circle-outline" size={40} color={COLORS.NEUTRAL.GRAY} />
+        }
+        titleLabel={'Success'}
+        message={
+          'Your requested has been recieved and will be Processed. You will recieve an email for further steps.'
+        }
+        modalVisible={statementModalSuccess}
+        setModalVisible={setStatementModalSuccess}
+        buttonLabelTwo={'Proceed'}
+        buttonOneOnPress={() => {}}
+        buttonTwoOnPress={() => {
+          setStatementModalSuccess(!statementModalSuccess);
+          setWithdrawalReason('');
+          setFundChoice('');
+          navigation.replace('DrawerMenu');
+        }}
+      />
+      <KeyboardAvoidingView behavior="height">
+        <SafeAreaView style={styles.wrapper}>
+          <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ ...styles.container }}
+          >
+            <Text style={{ ...FONTS.body4Bold, marginVertical: moderateScale(10) }}>
+              Please kindly fill the form below appropiately
+            </Text>
+            <CustomInput
+              placeholder={'Reason for Fund Movement'}
+              multiline={true}
+              onChangeText={(text: string) => setWithdrawalReason(text)}
+              autoCapitalize="none"
+              onFocus={() => handleError(null, 'withdrawalReason')}
+              error={errors.withdrawalReason}
+              editable={true}
+              dark={true}
+            />
+            <Text style={{ ...FONTS.body4Bold, marginTop: moderateScale(10) }}>To</Text>
+            <CustomInput
+              placeholder={'Fund Type'}
+              onChangeText={(text: string) => setFundChoice(text)}
+              autoCapitalize="none"
+              onFocus={() => handleError(null, 'fundChoice')}
+              error={errors.fundChoice}
+              editable={true}
+              dark={true}
+              keyboardType="numeric"
+            />
 
-          <Text style={{ ...FONTS.body4Bold, marginTop: moderateScale(20) }}>
-            Kindly Indicate how you would want your notification to be dispatched to you
-          </Text>
-          <View style={styles.dispatchChoice}>
-            <View style={styles.checkBoxWrapper}>
-              <Text style={styles.checkText}>EMAIL</Text>
-              <Checkbox
-                style={styles.checkbox}
-                value={emailChecked}
-                onValueChange={setEmailChecked}
-                color={emailChecked ? COLORS.PRIMARY.NORMAL : undefined}
-              />
+            <Text style={{ ...FONTS.body4Bold, marginTop: moderateScale(20) }}>
+              Kindly Indicate how you would want your notification to be dispatched to you
+            </Text>
+            <View style={styles.dispatchChoice}>
+              <View style={styles.checkBoxWrapper}>
+                <Text style={styles.checkText}>EMAIL</Text>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={emailChecked}
+                  onValueChange={setEmailChecked}
+                  color={emailChecked ? COLORS.PRIMARY.NORMAL : undefined}
+                />
+              </View>
+              <View style={styles.checkBoxWrapper}>
+                <Text style={styles.checkText}>SMS</Text>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={smsChecked}
+                  onValueChange={setSmsChecked}
+                  color={smsChecked ? COLORS.PRIMARY.NORMAL : undefined}
+                />
+              </View>
+              <View style={styles.checkBoxWrapper}>
+                <Text style={styles.checkText}>PHONE</Text>
+                <Checkbox
+                  style={styles.checkbox}
+                  value={phoneChecked}
+                  onValueChange={setPhoneChecked}
+                  color={phoneChecked ? COLORS.PRIMARY.NORMAL : undefined}
+                />
+              </View>
             </View>
-            <View style={styles.checkBoxWrapper}>
-              <Text style={styles.checkText}>SMS</Text>
-              <Checkbox
-                style={styles.checkbox}
-                value={smsChecked}
-                onValueChange={setSmsChecked}
-                color={smsChecked ? COLORS.PRIMARY.NORMAL : undefined}
-              />
-            </View>
-            <View style={styles.checkBoxWrapper}>
-              <Text style={styles.checkText}>PHONE</Text>
-              <Checkbox
-                style={styles.checkbox}
-                value={phoneChecked}
-                onValueChange={setPhoneChecked}
-                color={phoneChecked ? COLORS.PRIMARY.NORMAL : undefined}
-              />
-            </View>
-          </View>
 
-          <Button
-            onPress={() => {
-              validate();
-            }}
-            text={'Submit'}
-            customBtnStyle={styles.customBtnStyle}
-          />
-        </View>
-      </SafeAreaView>
+            <Button
+              onPress={() => {
+                validate();
+              }}
+              text={'Submit'}
+              customBtnStyle={styles.customBtnStyle}
+            />
+          </ScrollView>
+        </SafeAreaView>
+      </KeyboardAvoidingView>
     </View>
   );
 };
