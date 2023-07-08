cf730491f7ee4ba09904f41a3225daa9d31bef4d
Sendy Inieke <ini.e.sendy@gmail.com>
2023-07-08 12:52:27 +0100

    [feat-statement]:update: config Ip addresses

--- config.ts
+++ config.ts
@@ -1,6 +1,7 @@
 const BASE_API_URL = 'http://154.65.4.197:9912/CrusaderWebService/crusader/pensions';
 // const BASE_API_URL = 'http://154.65.4.193:9912/CrusaderWebService/crusader/pensions';
-const BASE_STATEMENT_API = 'http://132.10.100.221:9912/crusader/webservices/api/statement';
+// const BASE_STATEMENT_API = 'http://132.10.100.221:9912/crusader/webservices/api/statement';  local
+const BASE_STATEMENT_API = 'http://102.88.14.235:9912/crusader/webservices/api/statement';
 const USERNAME = 'cr8der';
 const PASSWORD = 'p@sWrd9';
 
